0a8d1f44ada46dec4ce95a74b8dab186ddd047cf
Ella Mae A <ellamae207@gmail.com>
2017-09-20 10:50:51 -1000

    passes 2 - complete

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,79 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
+/*module.exports = function( ceiling ) {
   // do work here
+  var num = 0;
+  function checker (){
+    for(var i=0; i<=ceiling; i++){
+      if(num % i === 0 ){
+        return false ;
+      }
+    }
+  }if(checker === false){
+    num++;
+  }
+
+};
+*/
+
+//6090ms
+module.exports = function( ceiling ) {
+  var num = 2;
+  function checker(){
+    for(var i=2; i<=ceiling; i++){
+      if(num % i !== 0){
+        return false;
+      }
+    }
+  }
+  while (checker() === false){
+    num ++;
+  }
+  return num;
+};
+
+console.log(module.exports(10));
 
 
-  return 0;
+
+
+//ED's Solution (6230ms)
+/*module.exports = function (ceiling){
+  let result;
+  while(!result);
+    let allFactorsPass = true;
+
+    for(let i=0; i<factors.length; i++){
+      if(num % factors[i] !== 0){
+        allFactorsPass = false;
+        break;
+      }
+    }
+
+  if(allFactorsPass){
+    result = num;
+  }else{
+    num++;
+  }
+};*/
+
+
+module.exports = function (ceiling){
+  let result;
+  while(!result);
+    let allFactorsPass = true;
+
+    for(let i=0; i<factors.length; i++){
+      if(num % factors[i] !== 0){
+        allFactorsPass = false;
+        break;
+      }
+    }
+
+  if(allFactorsPass){
+    result = num;
+  }else{
+    num++;
+  }
 };
